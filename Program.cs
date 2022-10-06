b4a51afd19bcb212cea64ab9d74714e6a4266384
eden2292 <kp798@student.aru.ac.uk>
2022-10-06 11:29:52 +0100

    Wrote program so days of the week are outputted.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿int day;
+
+Console.WriteLine("Please enter a number between 1 and 7");
+try
+{
+
+    day = Convert.ToInt32(Console.ReadLine());
+
+
+    switch (day)
+    {
+        case 1:
+            Console.WriteLine("Monday");
+            break;
+
+        case 2:
+            Console.WriteLine("Tuesday");
+            break;
+
+        case 3:
+            Console.WriteLine("Wednesday");
+            break;
+
+        case 4:
+            Console.WriteLine("Thursday");
+            break;
+
+        case 5:
+            Console.WriteLine("Friday");
+            break;
+
+        case 6:
+            Console.WriteLine("Saturday");
+            break;
+
+        case 7:
+            Console.WriteLine("Sunday");
+            break;
+
+        default:
+            Console.WriteLine("I said 1-7!");
+            break;
+    }
+
+}
+
+catch
+{
+    Console.WriteLine("Incorrect Input");
+}
+
